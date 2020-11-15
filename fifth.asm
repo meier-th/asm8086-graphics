cd6b6ef63b042e7dd950a908a6e2d339a55a0d10
meier-th <meier.d.th@gmail.com>
2020-11-15 14:45:51 +0300

    Fix steep line

--- fifth.asm
+++ fifth.asm
@@ -2,20 +2,25 @@
 .code
 
 mov ax,5       			
-int 10h			 
+int 10h			
 
-;mov cx, 40 ; length
-;mov ax, 103 ; x start
-;mov bx, 100 ; y
-;call draw_hbar
-;mov cx, 40 ; height
-;mov ax, 103 ; x
-;mov bx, 100 ; y start
-;call draw_vbar
+mov cx, 40 ; length
+mov ax, 103 ; x start
+mov bx, 100 ; y
+call draw_hbar
+mov cx, 40 ; height
+mov ax, 103 ; x
+mov bx, 100 ; y start
+call draw_vbar
 mov bl, 10
 mov bh, 20
-mov cl, 2 ; dx
-mov ch, 2 ; dy
+mov cl, 10
+mov ch, 5
+call draw_line
+mov bl, 10
+mov bh, 20
+mov cl, 7 ; dx
+mov ch, 20 ; dy
 call steep_line
 
 xor ax,ax				
@@ -66,7 +71,7 @@ mov ax,0ba00h
 
 	draw:
 mov  es,ax          
-and ax, 0 
+and ax, 0
 mov al,10000000b
 shr al,cl
 mov dl, 0c0h ; mask
@@ -132,11 +137,11 @@ mov cx, dx
 and dx, 0
 and ax, 0
 add ah, bl
-add ah, cl
-mov dl, ah
-mov al, cl
-shl al, 1
-sub al, cl
+add ah, cl ; x0+dx
+mov dl, ah ; x0+dx
+mov al, ch ; dy
+shl al, 1 ; 2dy
+sub al, cl ; 2dy - dx (d0)
 mov ah, dl
 and dl, 0
 push ax ; xf,d0 |xf,d0| ...
@@ -152,17 +157,17 @@ pop dx ; dy,dx |xf,di-1| ...
 pop cx ; xf,di-1 | ...
 push cx ; |xf,di-1| ...
 cmp al, ch
-je ex
+je ex ; if x == xf
 inc ax
 and cl, 80h
 cmp cl, 0
-je inc_y
+je inc_y ; if (di-1) >= 0
 
 pop cx ; xf,di-1 | ...
-push dx
+push dx ; |dy,dx| ...
 and dl, 0
 shl dh, 1 ; 2*dy
-add cl, dh ; di
+add cl, dh ; di = di-1 + 2dy
 pop dx ; dy,dx
 push cx ; |xf,di| ...
 push dx ; |dy,dx|xf,di| ...
@@ -176,10 +181,10 @@ mov ah, cl
 pop cx ; +-1
 cmp cx, -1
 je dec_y
-inc bx ; yi
+inc bl ; yi
 jmp after_y
 	dec_y:
-dec bl
+dec bl ; yi
 	after_y:
 push cx
 mov cl, ah
@@ -188,8 +193,8 @@ and ah, 0
 and bh, 0
 push cx ; |xf,di-1| ...
 and cx, 0
-mov cl, dh
-sub cl, dl
+mov cl, dh ; dy
+sub cl, dl ; dy-dx
 shl cl, 1 ; 2(dy-dx)
 mov bh, al ; bx: xiyi
 and ax, 0
@@ -213,34 +218,30 @@ and dx, 0
 ret
 draw_line endp
 
-
-
-
-; seems right but does not work
 steep_line proc ; bl - x0, bh - y0, cl - dx, ch - dy
-mov dx, cx
-and ch, 80h
-cmp ch, 0
+mov dx, cx ; dydx
+and cl, 80h
+cmp cl, 0
 push 1
-je s_cont
-xor dh, 0ffh
-inc dh
+je s_cont ; if dx >= 0
+xor dl, 0ffh
+inc dl ; dx = |dx|
 pop cx
 push -1
 	s_cont:
-mov cx, dx
+mov cx, dx ; dydx
 and dx, 0
 and ax, 0
 add ah, bh
-add ah, ch
-mov dl, ah
-mov al, cl
-shl al, 1
-sub al, ch
+add ah, ch ; yf
+mov dl, ah 
+mov al, cl ; dx
+shl al, 1 ; 2dx
+sub al, ch ; 2dx-dy
 mov ah, dl
 and dl, 0
-push ax ; xf,d0 |xf,d0| ...
-push cx ; dy,dx |dy,dx|xf,d0| ...
+push ax ; yf,d0 |yf,d0| ...
+push cx ; dy,dx |dy,dx|yf,d0| ...
 
 and ax, 0
 and cx, 0
@@ -249,63 +250,56 @@ mov bl, bh ; y0
 and bh, 0
 	s_dot:
 call draw_dot
-pop dx ; dy,dx |xf,di-1| ...
-pop cx ; xf,di-1 | ...
-push cx ; |xf,di-1| ...
+pop dx ; dy,dx |yf,di-1| ...
+pop cx ; yf,di-1 | ...
+push cx ; |yf,di-1| ...
 cmp bl, ch
-je s_ex
-
-pop cx; xf,di-1 | ...
-mov bh, ch
-mov ah, cl
-pop cx ; +-1
-cmp cx, -1
-je s_dec_y
-inc bl
-jmp s_after_y
-	s_dec_y:
-dec bl
-	s_after_y:
-push cx
-mov cl, ah
-mov ch, bh
-and ah, 0
-and bh, 0
-push cx ; |xf,di-1| ...
+je s_ex ; if y == yf
+inc bl ; yi
 
 and cl, 80h
 cmp cl, 0
-je inc_x
+je inc_x ; if di-1 >= 0
 
-pop cx ; xf,di-1 | ...
-push dx
+pop cx ; yf,di-1 | ...
+push dx ; |dy,dx| ...
 and dh, 0
 shl dl, 1 ; 2*dx
-add cl, dl ; di
+add cl, dl ; di = di-1 + 2dx
 pop dx ; dy,dx
-push cx ; |xf,di| ...
-push dx ; |dy,dx|xf,di| ...
+push cx ; |yf,di| ...
+push dx ; |dy,dx|yf,di| ...
 and dx, 0
 and cx, 0
 jmp s_dot
-	inc_x:
-inc ax ; xi
-
-mov cl, dl
-sub cl, dh
-shl cl, 1 ; 2(dx-dy)
-mov bh, al ; bx: xiyi
-and ax, 0
-pop ax ; xf,di-1 | ...
-add al, cl ; xf,di
-push ax ; |xf,di| ...
-and ax, 0
-mov al, bh
+	inc_x: ; |yf,di-1| ... al : x, bl: y, cx: yf,di-1, dx: dy,dx
+pop cx
+mov ah, cl
+mov bh, ch
+pop cx ; +-1
+add al, cl ; xi
+push cx ; +-1
+mov cl, ah
+mov ch, bh
+push cx ; |yf,di-1| ...
+and ah, 0
 and bh, 0
-push dx ; |dy,dx|xf,di| ...
-and dx, 0
+mov bh, al ; bx: xy
+and al, 0
+mov al, dl ; dx
+sub al, dh ; dx-dy
+shl al, 1 ; 2(dx-dy)
+add al, cl ; di = di-1 + x(dx-dy)
+mov cl, al
+pop ax ; |...
+push cx ; |yf,di| ...
+push dx ; |dy,dx|yf,di| ...
+and ax, 0
+mov al, bh; ax = 0x
+and bh, 0 ; bx = 0y
 and cx, 0
-jmp dot
+and dx, 0
+jmp s_dot
 	s_ex:
 pop ax
 pop ax
